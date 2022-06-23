ae3c01785283a68ff4e1dac2514d2d2b43085cae
AishSI <aishsi@gmail.com>
2022-06-23 09:59:01 +0700

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+   let s = Math.abs(n) + '';
+   return [...s].reverse().join('');
+   // return n < 0 ? [...s].reverse().join('') * -1 : [...s].reverse().join('');
 }
